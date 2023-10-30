1e80bcfb03b4207923953d767160b98ccef25fb2
GabbyGreat <gabrieloranekwu@gmail.com>
2023-10-30 18:22:18 +0100

    :ehfj

--- second.py
+++ second.py
@@ -1,10 +1,2 @@
 print('Second')
 print("Another")
-# print("Thirsd") edweqsm,scdhjde
-# wdc;frejkrekl
-print("Fourth")
-print("Fourth")
-print("Fourth")
-print("Fourth")
-print("Fourth")
-print("Fourth")
